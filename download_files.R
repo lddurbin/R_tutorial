7846d2249ad781402335e0bf7f9a6b5b080020a9
lddurbin <l.d.durbin@gmail.com>
2020-12-14 14:03:16 +1300

    Unzip files. Store files in dedicated folders.

--- download_files.R
+++ download_files.R
@@ -3,4 +3,7 @@ library("tidyverse")
 
 target_urls <- c("https://www.bl.uk/britishlibrary/~/media/bl/global/services/collection%20metadata/pdfs/bnb%20records%20rdf/bnbrdf_n3618.zip?la=en&hash=AA432EC232641BA593846EB29440A29F", "https://www.bl.uk/britishlibrary/~/media/bl/global/services/collection%20metadata/pdfs/bnb%20records%20rdf/bnbrdf_n3619.zip?la=en&hash=425EF29835CA284A9F7C980BF443E4E8", "https://www.bl.uk/britishlibrary/~/media/bl/global/services/collection%20metadata/pdfs/bnb%20records%20rdf/bnbrdf_n3620.zip?la=en&hash=DC5A0A827772208859EAFF59B8FB3128")
 target_files <- basename(target_urls) %>% str_replace("(?<=.zip).*$","")
-download.file(target_urls, destfile = target_files, method = "libcurl")
+download.file(target_urls, destfile = paste0("raw data/zipped/", target_files), method = "libcurl")
+
+zipped_files <- list.files("raw data/zipped", pattern = ".zip$", full.names = TRUE)
+walk(zipped_files, unzip, exdir = "raw data/unzipped")
